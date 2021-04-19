bc6a3b5b2cada5cb2361a929b0c68eff560e9758
Guilherme Siqueira <guilhersiqueira@gmail.com>
2021-04-19 12:55:03 -0300

    menu

--- central/src/monitoring.c
+++ central/src/monitoring.c
@@ -141,11 +141,11 @@ void set_output(int code)
         request_GPIO_toggle(port, response);
         sscanf(response, "%c", &res);
 
-        if (res != 'G')
-        {
-            _output = output;
-            print_data(_output);
-        }
+        // if (res != 'G')
+        // {
+        //     _output = output;
+        //     print_data(_output);
+        // }
         
     }
 }
